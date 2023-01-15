c1bdce9bf105263627dd94165ac42c850a9e46c2
Ryota Mizumaki <statham.programming@gmail.com>
2023-01-15 15:14:20 +0900

    implement basic feature

--- preload.js
+++ preload.js
@@ -3,15 +3,5 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("magicalClipboard", {
   prev: (callback) => ipcRenderer.on("clipboard-write-prev", callback),
   forward: (callback) => ipcRenderer.on("clipboard-write-forward", callback),
-});
-
-window.addEventListener("DOMContentLoaded", () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  };
-
-  for (const dependency of ["chrome", "node", "electron"]) {
-    replaceText(`${dependency}-version`, process.versions[dependency]);
-  }
+  writeText: (value) => ipcRenderer.send("clipboard-writeText", value),
 });
--- renderer.js
+++ renderer.js
@@ -1,11 +1,41 @@
-let count = 0;
+let values = [];
+let selectedIndex = 0;
 
-window.magicalClipboard.prev((event) => {
-  count--;
-  event.sender.send("clipboard-writeText", count);
+const input = document.getElementById("array-input");
+const button = document.getElementById("set-button");
+const errorText = document.getElementById("error-contents");
+const valuesText = document.getElementById("values-contents");
+const valuesLengthText = document.getElementById("values-length-contents");
+const selectedValueText = document.getElementById("selected-value-contents");
+const selectedIndexText = document.getElementById("selected-index-contents");
+
+const updateSelectedValue = (newIndex) => {
+  selectedIndex = newIndex;
+  const newVal = values[selectedIndex];
+  window.magicalClipboard.writeText(newVal);
+  selectedValueText.innerText = newVal;
+  selectedIndexText.innerText = newIndex;
+};
+
+window.magicalClipboard.prev(() => {
+  if (selectedIndex > 0) {
+    updateSelectedValue(selectedIndex - 1);
+  }
 });
 
-window.magicalClipboard.forward((event) => {
-  count++;
-  event.sender.send("clipboard-writeText", count);
+window.magicalClipboard.forward(() => {
+  if (selectedIndex + 1 < values.length) {
+    updateSelectedValue(selectedIndex + 1);
+  }
 });
+
+const csvTextToArray = (text) => {
+  return text.split(",").map((t) => t.trimStart());
+};
+
+button.onclick = () => {
+  values = csvTextToArray(input.value);
+  valuesText.innerText = JSON.stringify(values);
+  valuesLengthText.innerText = values.length;
+  updateSelectedValue(0);
+};
